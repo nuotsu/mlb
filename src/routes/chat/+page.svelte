4cd7db0824e52426053d1347ecd9371bd52a8ff3
Mitchell Christ <mitchell@nuotsu.dev>
2026-01-24 19:45:32 -0800

    add route

---
+++ src/routes/chat/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Metadata from '$ui/metadata.svelte'
+</script>
+
+<Metadata title="Ask Mitch | MLB.TheOhtani.com" description="Ask Mitch about anything MLB" />
+
+<section class="p-ch text-center">
+	<h1>Coming soon...</h1>
+</section>
